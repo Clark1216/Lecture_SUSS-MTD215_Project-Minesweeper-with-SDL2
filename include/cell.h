91285518f113fdc7f1b66a5980e1de0d95ff8e42
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2021-07-13 23:00:32 +0100

    added cell variables and functions

--- include/cell.h
+++ include/cell.h
@@ -6,8 +6,23 @@
 
 //A Cell is a button
 class Cell : public Button {
+    public:
+        static SDL_Color sCellColour;
+        static SDL_Color sPressedCellColour;
+        static SDL_Texture** sTextureOfNumbers;
+        static SDL_Texture* sFlagTexture;
+        static SDL_Texture* sBombTexture;
+
     private:
+        bool mBomb;
+        int mNumber;
+        bool mPressed;
+        bool mFlag;
 
     public:
         Cell(const SDL_Rect& rect, const SDL_Color& colour);
+        void plantBomb();
+        void plantNumber(int number);
+        void setPressed();
+        void setFlag();
 };
--- src/cell.cpp
+++ src/cell.cpp
@@ -1,6 +1,35 @@
 #include "cell.h"
 
-Cell::Cell(const SDL_Rect& rect, const SDL_Color& colour)
-    : Button(rect, colour) {
+SDL_Color Cell::sCellColour = {0, 0, 0, 0};
+SDL_Color Cell::sPressedCellColour = {0, 0, 0, 0};
+SDL_Texture** Cell::sTextureOfNumbers = nullptr;
+SDL_Texture* Cell::sFlagTexture = nullptr;
+SDL_Texture* Cell::sBombTexture = nullptr;
 
+Cell::Cell(const SDL_Rect& rect, const SDL_Color& colour)
+    : Button(rect, colour), mNumber(0), mPressed(false), mFlag(false), mBomb(false) {
+
+}
+
+void Cell::plantBomb() {
+    mBomb = true;
+}
+
+void Cell::plantNumber(int number) {
+    mNumber = number;
+}
+
+void Cell::setPressed() {
+    if (!mPressed && !mFlag) {
+        mPressed = true;
+        mColour = Cell::sPressedCellColour;
+        setTexture(mNumber ? Cell::sTextureOfNumbers[mNumber] : mBomb ? Cell::sBombTexture : mTexture);
+    }
+}
+
+void Cell::setFlag() {
+    if (!mPressed) {
+        mFlag = !mFlag;
+        setTexture(mFlag ? Cell::sFlagTexture : nullptr);
+    }
 }
