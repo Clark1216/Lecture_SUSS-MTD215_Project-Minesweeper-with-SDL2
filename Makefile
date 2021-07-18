b12ecd00ac5396db3e835c066934ce8fbd83ef98
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2021-07-18 02:01:13 +0100

    added menu

--- Makefile
+++ Makefile
@@ -23,11 +23,14 @@ LINKER_FLAGS = -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf
 OBJ_NAME = main.exe
 
 #This is the target that compiles our executable
-$(BIN_PATH)\$(OBJ_NAME) :  $(BIN_PATH)\main.o $(BIN_PATH)\loadTexture.o $(BIN_PATH)\UI_element.o $(BIN_PATH)\button.o $(BIN_PATH)\cell.o $(BIN_PATH)\board.o
+$(BIN_PATH)\$(OBJ_NAME) :  $(BIN_PATH)\main.o $(BIN_PATH)\menu.o $(BIN_PATH)\board.o $(BIN_PATH)\loadTexture.o $(BIN_PATH)\UI_element.o $(BIN_PATH)\button.o $(BIN_PATH)\cell.o $(BIN_PATH)\board.o
 	$(CC) $^ $(LIBRARY_PATHS) $(LINKER_FLAGS) -o $@
 
-$(BIN_PATH)\main.o : $(SRC_PATH)\main.cpp $(INC_PATH)\board.h
+$(BIN_PATH)\main.o : $(SRC_PATH)\main.cpp $(INC_PATH)\board.h $(INC_PATH)\menu.h $(INC_PATH)\cell.h
 	$(CC) -c $< $(INCLUDE_PATHS) -o $@
+
+$(BIN_PATH)\menu.o : $(SRC_PATH)\menu.cpp $(INC_PATH)\menu.h $(INC_PATH)\menu.h $(INC_PATH)\loadTexture.h $(INC_PATH)\button.h
+	$(CC) -c $< $(INCLUDE_PATHS) -o $@	
 
 $(BIN_PATH)\board.o : $(SRC_PATH)\board.cpp $(INC_PATH)\board.h $(INC_PATH)\loadTexture.h
 	$(CC) -c $< $(INCLUDE_PATHS) -o $@	
---
+++ src/menu.cpp
@@ -0,0 +1,81 @@
+#include "menu.h"
+
+Menu::Menu()
+    : mEasyTexture(nullptr), mMediumTexture(nullptr), mHardTexture(nullptr), mChosenButton(nullptr) {
+
+}
+
+void Menu::createButtons(SDL_Renderer* renderer) {
+    //load font
+    const int FONT_SIZE = 30;
+    SDL_Color FONT_COLOUR = {255, 255, 255, 255}; //White
+	TTF_Font* font = getFont(FONT_SIZE);
+
+    //Load texture
+    SDL_Texture* mEasy = loadTexture(renderer, font, FONT_COLOUR, "Easy");
+    SDL_Texture* mMedium = loadTexture(renderer, font, FONT_COLOUR, "Medium");
+    SDL_Texture* mHard = loadTexture(renderer, font, FONT_COLOUR, "Hard");
+    SDL_Texture* menuTextures[3] = {mEasy, mMedium, mHard};
+
+    //Define button attributes
+    const int MAX_BUTTONS = 3;
+    const int BUTTON_GAP = 10;
+    const int BUTTON_WIDTH = 153;
+    const int BUTTON_HEIGHT = 130;
+    const SDL_Color BUTTON_COLOUR = {145, 145, 145, 255};
+
+    //Define max rows and max columns for each difficulty and bombs
+    BoardDetails easy(9, 9, 10);
+    BoardDetails medium(16, 16, 40);
+    BoardDetails hard(16, 30, 99);
+    BoardDetails boardDetails[MAX_BUTTONS] = {easy, medium, hard};
+
+    //Starting coordinates
+	int x = BUTTON_GAP;
+	int y = BUTTON_GAP;
+	//Create buttons
+	for (int i = 0; i < MAX_BUTTONS; ++i) {
+        SDL_Rect rect = {x, y, BUTTON_WIDTH, BUTTON_HEIGHT};
+        DifficultyButton button(rect, BUTTON_COLOUR);
+        button.setTexture(menuTextures[i]);
+        button.boardDetails = boardDetails[i];
+        mDifficultyButtons.push_back(button);
+        x += BUTTON_WIDTH + BUTTON_GAP;
+		y = BUTTON_GAP;
+	}
+}
+
+void Menu::handleMouseDown(const SDL_Event& event, bool& renderFlag) {
+    int mouseX, mouseY;
+    SDL_GetMouseState(&mouseX, &mouseY);
+    for (auto& button : mDifficultyButtons) {
+        if (button.isMouseInside(mouseX, mouseY)) {
+            if (event.button.button == SDL_BUTTON_LEFT) {
+                mChosenButton = &button;
+            }
+        }
+    }
+}
+
+bool Menu::handleState(BoardDetails& boardDetails) {
+    if (mChosenButton != nullptr) {
+        boardDetails = mChosenButton->boardDetails;
+        return true;
+    }
+    return false;
+}
+
+void Menu::render(SDL_Renderer* renderer) {
+    for (auto& button : mDifficultyButtons) {
+        button.render(renderer);
+    }
+}
+
+Menu::~Menu() {
+    SDL_DestroyTexture(mEasyTexture);
+	SDL_DestroyTexture(mMediumTexture);
+    SDL_DestroyTexture(mHardTexture);
+	mEasyTexture = nullptr;
+    mMediumTexture = nullptr;
+    mHardTexture = nullptr;
+}
