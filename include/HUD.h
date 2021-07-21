d3a686bcb9fc1567f7ed912eaa0520ac08ff6236
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2021-07-21 20:26:28 +0100

    first commit

---
+++ include/HUD.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include "loadTexture.h"
+
+//Contains number of flags(bombs/mines) remaining, a timer, and a menu button
+class HUD {
+    private:
+
+    public:
+        
+};
